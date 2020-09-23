53f685296fbc95bac747572bc5d54186bc948265
Hyung Kim <hjkmines@gmail.com>
2020-09-22 21:26:58 -0600

    adds passport auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://tkim123:123@cluster0.2urup.mongodb.net/bugview?retryWrites=true&w=majority"
-}
+    mongoURI: "mongodb+srv://tkim123:123@cluster0.2urup.mongodb.net/bugview?retryWrites=true&w=majority", 
+    secretOrKey: 'secret'
+}; 
